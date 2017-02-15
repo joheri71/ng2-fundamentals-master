6420359193e1190d4f188bc1b9c314b940f274b0
Johan Eriksson <joheri71@hotmail.com>
2017-02-15 16:26:14 +0100

    T.o.m Using Template variables to interact with child component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {EventsAppComponent} from './events-app.component'
+import {EventsListComponent} from './events/events-list.component'
+import {EventsThumbnailComponent} from './events/events-thumbnail.component'
 
 @NgModule({
 imports: [BrowserModule],
-declarations: [EventsAppComponent],
+declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventsThumbnailComponent],
 bootstrap: [EventsAppComponent]
 })
 export class AppModule{}
